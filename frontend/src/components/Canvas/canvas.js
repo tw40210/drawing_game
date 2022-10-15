6060606c4a17c58b96a8cffc5a161184cd7f0318
tw40210 <tw40210@gmail.com>
2022-10-15 17:36:52 +0800

    fixing layout, finish canvas save load

--- frontend/src/components/Canvas/canvas.js
+++ frontend/src/components/Canvas/canvas.js
@@ -1,27 +1,71 @@
-import React from "react";
+import React, { useRef} from "react";
 import useStyles from './styles';
-import { useOnDraw } from "../../hooks/Hooks";
+import { useCanvas } from "../../hooks/Hooks";
+import {   Grow, Grid, Paper, Button } from "@material-ui/core";
 
 const Canvas = ({width, height}) => {
-    const classes = useStyles()
-    const onDraw = (ctx, point) =>{
-        ctx.fillStyle = '#000000';
+    const classes = useStyles();
+    const canvasRef = useRef(null);
+    let img = new Image();
+    img.onload = function () {
+        canvasRef.current.getContext('2d').clearRect(0, 0, width, height);
+        canvasRef.current.getContext('2d').drawImage(img, 0, 0, width, height);
+    };
+
+    const canvasName = "canvasImg";
+
+    const onDraw = (ctx, point, prevPoint) =>{
+        // console.log(point, prevPoint);
+        drawLine(point, prevPoint, 5, "#000000", ctx);
+    }
+
+    const drawLine = (start, end, width, color, ctx)=>{
+        start = start ?? end;
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 2, 0, 2*Math.PI);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(start.x, start.y, 2, 0, 2*Math.PI);
         ctx.fill();
 
     }
 
+    
 
-    const setCanvasRef = useOnDraw(onDraw)
+    const setCanvasRef = useCanvas(onDraw, canvasRef)
+    const handleSaveCtx = ()=>{
+        localStorage.setItem("canvasImg", canvasRef.current.toDataURL());
+    };
+
+    const handleLoadCtx = ()=>{
+        const dataURL = localStorage.getItem(canvasName);
+        img.src = dataURL;
+
+    };
+    
     return (
-        <canvas
-            width={`${width}px`}
-            height={`${height}px`}
-            className={classes.canvas}
-            ref={setCanvasRef}
-            
-        />
+        <Paper>
+            <Grow in>
+                <Grid container justifyContent="space-between"  spacing={3}  className={classes.gridContainer}>
+                    <Grid className={classes.grid} item >
+                        <canvas width={`${width}px`} height={`${height}px`} className={classes.canvas} ref={setCanvasRef}/>
+                    </Grid>
+                    <Grid className={classes.grid} item >
+                        <Button onClick={handleSaveCtx}>SAVE</Button>
+                    </Grid>
+                    <Grid className={classes.grid} item >
+                        <Button onClick={handleLoadCtx}>LOAD</Button>
+                    </Grid>
+                </Grid>
+            </Grow>
+        </Paper>
+        
+
 
     )
 }
--- frontend/src/hooks/Hooks.js
+++ frontend/src/hooks/Hooks.js
@@ -1,12 +1,46 @@
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
-export const useOnDraw = (onDraw)=>{
-    const canvasRef = useRef(null);
+// export const useCanvas =()=>{
+//     const canvasRef = useRef(null);
+//     console.log(canvasRef.current);
+//     const getCanvas = ()=>{
+//         console.log(canvasRef.current);
+//     }
 
+
+
+//     return getCanvas;
+
+// }
+
+
+export const useCanvas = (onDraw, canvasRef)=>{
+    // const canvasRef = useRef(null);
+    const isDrawingRef = useRef(false);
+
+    const mouseMoveListenerRef = useRef(null);
+    const mouseUpListenerRef = useRef(null);
+    const mouseDownListenerRef = useRef(null);
+
+    const prevPointRef = useRef(null);
+
+    useEffect(() =>{
+        return ()=> {
+            if (mouseMoveListenerRef.current) window.removeEventListener("mousemove", mouseMoveListenerRef.current);
+            if (mouseUpListenerRef.current) window.removeEventListener("mouseup", mouseUpListenerRef.current);
+        }
+
+    }, [])
+    
     const setCanvasRef = (ref)=>{
         if (!ref) return;
+        if (canvasRef.current) canvasRef.current.removeEventListener("mousedown", mouseDownListenerRef.current)
         canvasRef.current = ref;
+        // console.log("CCCanvans:")
+        // console.log(canvasRef.current)
         initMouseListener();
+        initMouseDownListener();
+        initMouseUpListener();
     }
 
     const getCanvasPoint = (clientX, clientY) =>{
@@ -21,15 +55,39 @@ export const useOnDraw = (onDraw)=>{
 
     const initMouseListener = ( ) =>{
         const mouseMoveListener = (e) => {
-            const point = getCanvasPoint(e.clientX, e.clientY);
-            const ctx = canvasRef.current.getContext('2d');
-            if (onDraw) onDraw(ctx, point);
-            console.log({x: point.x, y: point.y});
+            if (isDrawingRef.current){
+                const point = getCanvasPoint(e.clientX, e.clientY);
+                const ctx = canvasRef.current.getContext('2d');
+                prevPointRef.current = prevPointRef.current ?? point;
+                if (onDraw) onDraw(ctx, point, prevPointRef.current);
+                prevPointRef.current = point;
+            }
         }
-
+        mouseMoveListenerRef.current = mouseMoveListener
         window.addEventListener("mousemove", mouseMoveListener)
     }
 
+    const initMouseDownListener = ( ) => {
+        const listener = () => {
+            if (!canvasRef.current) return;
+            isDrawingRef.current = true;
+        }
+        
+        mouseDownListenerRef.current = listener
+        canvasRef.current.addEventListener("mousedown", listener)
+    }
 
+    const initMouseUpListener = ( ) => {
+        const listener = () => {
+            if (!canvasRef.current) return;
+            isDrawingRef.current = false;
+            prevPointRef.current = null;
+        }
+
+        mouseUpListenerRef.current = listener
+        window.addEventListener("mouseup", listener)
+    }
+
+    
     return setCanvasRef;
 }
